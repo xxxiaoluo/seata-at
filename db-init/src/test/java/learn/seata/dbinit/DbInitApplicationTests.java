bdf6957b15d9b03b4f3088051110fcf7c967b917
Roy <1581950168@qq.com>
2023-02-26 20:11:45 +0800

    c

---
+++ db-init/src/test/java/learn/seata/dbinit/DbInitApplicationTests.java
@@ -0,0 +1,14 @@
+package learn.seata.dbinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
